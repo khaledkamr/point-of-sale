6a5f5eba0c3f3b6f8744148a6392c0e7ac9a689a
Khaled Kamr <200013171@must.edu.eg>
2025-12-18 13:39:02 +0200

    modified toast notification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -424,11 +424,6 @@
             }
 
             createToast(message, type = 'info', duration = 5000) {
-                const toast = document.createElement('div');
-                toast.className =
-                    `toast flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow-lg border border-gray-200`;
-                toast.setAttribute('role', 'alert');
-
                 const iconConfig = {
                     success: {
                         bgColor: 'bg-green-100',
@@ -454,15 +449,19 @@
 
                 const config = iconConfig[type] || iconConfig.info;
 
+                const toast = document.createElement('div');
+                toast.className = `toast flex items-center w-full max-w-xs p-4 text-gray-500 ${config.bgColor} rounded-lg shadow-lg border border-gray-200`;
+                toast.setAttribute('role', 'alert');
+
                 toast.innerHTML = `
-                    <div class="inline-flex items-center justify-center shrink-0 w-8 h-8 ${config.textColor} ${config.bgColor} rounded-lg">
+                    <div class="inline-flex items-center justify-center shrink-0 w-5 h-5 ${config.textColor} rounded-xl">
                         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             ${config.icon}
                         </svg>
                         <span class="sr-only">${type} icon</span>
                     </div>
                     <div class="ms-3 text-sm font-semibold text-gray-700">${message}</div>
-                    <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 toast-close" aria-label="Close">
+                    <button type="button" class="ms-auto -mx-1.5 -my-1.5 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:inline-flex items-center justify-center h-8 w-8 toast-close" aria-label="Close">
                         <span class="sr-only">Close</span>
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
